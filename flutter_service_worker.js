22b6dbcf998864ee2e362e687f37aae662df1b39
Aik Kalantarian <aik.kalantarian@woven-planet.global>
2021-11-11 06:25:17 -0800

    fdas

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,20 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "ffb0a46fca57c3c2b8da288a9009255a",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+  "main.dart.js": "9d620ee82123f2f4900e7a2ace026d5c",
+"assets/NOTICES": "e409d45ea3cdc9c7880b0d997ef174f8",
+"assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "134ccd4ffbaa1139dd24ad677de72111",
-"/": "134ccd4ffbaa1139dd24ad677de72111",
-"main.dart.js": "555dd16c589b7b33cefde50c8a22ae0a",
-"manifest.json": "295aae3a7b134e23c65f0fd3e36bafc7",
-"version.json": "086c6c387fb53c894cb35cfd2b8a01f2"
+"index.html": "499b0969e936bf3bd37fcd3d1527d51f",
+"/": "499b0969e936bf3bd37fcd3d1527d51f",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"manifest.json": "40fd70e18a7abf00c55eae7d8c3879e9",
+"version.json": "e65beac21cb6f051398703e48bd2fbab"
 };
 
 // The application shell files that are downloaded before a service worker can
